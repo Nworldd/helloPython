b5d7df8d0f2008440aade79776b72d2f46911dcb
Nworldd <schlkim2022@gmail.com>
2022-10-16 21:01:24 +0900

    3번째.

--- DemoTest.java
+++ DemoTest.java
@@ -8,11 +8,7 @@ public class DemoTest {
 
 	@Test
 	public void test() {
-<<<<<<< HEAD
 		fail("나도");
-=======
-		fail("아직정하지 않았다");
->>>>>>> branch 'main' of https://github.com/Nworldd/helloPython.git
 	}
 
 }
